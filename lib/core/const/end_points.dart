9a662a4323ac078793e23491343bec1202e61ea9
Andry <andrysadonis@gmail.com>
2023-01-17 15:41:28 -0500

    adding baseUrl for flags

--- lib/core/const/end_points.dart
+++ lib/core/const/end_points.dart
@@ -2,3 +2,4 @@ const geoCodingEndPoint = 'geo/1.0/direct';
 const nextDaysEndPoint = 'data/2.5/forecast';
 const currentWeatherEndPoint = 'data/2.5/weather';
 const baseUrl = 'api.openweathermap.org';
+const baseUrlFlags = 'https://countryflagsapi.com/png/';
