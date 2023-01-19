4c91b1e7847d5c2dae1b66233dec4d1ed0f63c59
Andry <andrysadonis@gmail.com>
2023-01-19 00:22:34 -0500

    updating shared preferences

--- lib/core/platfoms/share_preferences_handler.dart
+++ lib/core/platfoms/share_preferences_handler.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesManager {
   final _unit = 'units_value';
   final SharedPreferences sharedPreferences;
-
+  final _order_days = 'order_days';
   SharedPreferencesManager(this.sharedPreferences);
   Future<void> setUnitValue(bool value) async {
     await sharedPreferences.setBool(_unit, value);
@@ -11,7 +11,15 @@ class SharedPreferencesManager {
 
   Future<bool> getUnitValue() async {
     final value = sharedPreferences.getBool(_unit);
+    return value ?? false;
+  }
 
+  Future<void> setOrderValue(bool value) async {
+    await sharedPreferences.setBool(_order_days, value);
+  }
+
+  Future<bool> getOrderValue() async {
+    final value = sharedPreferences.getBool(_order_days);
     return value ?? false;
   }
 }
