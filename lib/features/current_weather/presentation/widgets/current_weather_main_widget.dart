debcabff4ac7610dcb818e1d73ad1ac93ebcec53
Andry <andrysadonis@gmail.com>
2023-01-19 10:06:05 -0500

    adding variable currentWeather

--- lib/features/current_weather/presentation/widgets/current_weather_main_widget.dart
+++ lib/features/current_weather/presentation/widgets/current_weather_main_widget.dart
@@ -1,146 +1,103 @@
-import 'dart:async';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app/core/exeptions.dart';
-import 'package:weather_app/core/weather_ui/weather_cubit.dart';
 import 'package:weather_app/features/current_weather/presentation/widgets/current_weather_variable_widget.dart';
 
-import '../bloc/current_weather_bloc.dart';
+import '../../../settings/presentation/cubit/settings_unit_cubit.dart';
+import '../../domain/entities/current_weather_entity.dart';
 
 class CurrentWeatherMain extends StatelessWidget {
+  final CurrentWeatherModel currentWeatherModel;
+
   const CurrentWeatherMain({
     super.key,
+    required this.currentWeatherModel,
   });
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      child: BlocBuilder<CurrentWeatherBloc, CurrentWeatherState>(
-        builder: (context, state) {
-          final letter = context.read<UnitCubit>().state ? '°C' : '°F';
-          if (state is CurrentWeatherInitialState) {
-            return const Center(
-              child: Text(
-                'Initial',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            );
-          }
-          if (state is CurrentWeatherLoadingState) {
-            return const Text(
-              'Cargando clima',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            );
-          }
-          if (state is CurrentWeatherLoadedState) {
-            return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(12),
-                      color: const Color.fromRGBO(32, 35, 41, 1),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    '${state.currentWeatherModel.main.temp} $letter',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: size.width * 0.15,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    '${state.currentWeatherModel.weather.first.main}, \n${state.currentWeatherModel.weather.first.description}',
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                      color: const Color.fromARGB(
-                                          255, 200, 200, 201),
-                                      //color: Colors.white,
-                                      fontSize: size.width * 0.07,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: CachedNetworkImage(
-                                  imageUrl:
-                                      'https://openweathermap.org/img/wn/${state.currentWeatherModel.weather.first.icon}@2x.png',
-                                  width: 80,
-                                )),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(12),
-                      color: const Color.fromRGBO(32, 35, 41, 1),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        CurrentWeatherColumns(
-                          icon: Icons.wind_power,
-                          value: '${state.currentWeatherModel.wind.speed} m/s',
-                          cat: 'Wind',
-                        ),
-                        CurrentWeatherColumns(
-                          icon: Icons.web,
-                          value: '${state.currentWeatherModel.main.humidity}%',
-                          cat: 'Humidity',
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            );
-          }
-          if (state is TimeoutException) {
-            return const Text('Reintente de nuevo');
-          }
-          if (state is NoInternetException) {
-            return Text(
-              (state as NoInternetException).message,
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-            );
-          }
-          return const Text(
-            'Ups, ha ocurrido un errorrr',
-            style: TextStyle(
-              color: Colors.white,
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusDirectional.circular(12),
+              color: const Color.fromRGBO(32, 35, 41, 1),
             ),
-          );
-        },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        BlocBuilder<UnitCubit, bool>(
+                          builder: (context, state) {
+                            return Text(
+                              '${currentWeatherModel.main.temp.round()} ${state ? '°C' : '°F'}',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: size.width * 0.15,
+                                  fontWeight: FontWeight.bold),
+                            );
+                          },
+                        ),
+                        Text(
+                          '${currentWeatherModel.weather.first.main}, \n${currentWeatherModel.weather.first.description}',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 200, 200, 201),
+                            //color: Colors.white,
+                            fontSize: size.width * 0.07,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: CachedNetworkImage(
+                          imageUrl:
+                              'https://openweathermap.org/img/wn/${currentWeatherModel.weather.first.icon}@2x.png',
+                          width: 80,
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusDirectional.circular(12),
+              color: const Color.fromRGBO(32, 35, 41, 1),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CurrentWeatherColumns(
+                  icon: Icons.wind_power,
+                  value: '${currentWeatherModel.wind.speed} m/s',
+                  cat: 'Wind',
+                ),
+                CurrentWeatherColumns(
+                  icon: Icons.water_drop_rounded,
+                  value: '${currentWeatherModel.main.humidity}%',
+                  cat: 'Humidity',
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
