e976f631039b52409bf5195ee61eae53098458e3
Andry <andrysadonis@gmail.com>
2023-01-22 14:15:48 -0500

    updating internet checker
    Increassing time to throw NoInternetException

--- lib/core/platfoms/internet_checker.dart
+++ lib/core/platfoms/internet_checker.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:weather_app/core/exeptions.dart';
+
 class CheckInternetConnection {
   static Future<bool> checkIfHaveInternet() async {
     try {
       //https://use.opendns.com/
       final result = await InternetAddress.lookup('example.com').timeout(
-          const Duration(seconds: 10),
-          onTimeout: () =>
-              throw TimeoutException('Can\'t connect in 10 seconds.'));
+          const Duration(seconds: 20),
+          onTimeout: () => throw NoInternetException());
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
         return true;
       }
