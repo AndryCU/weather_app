31aa2d7e8a435a0fa21effb138d8fd01f16a79e8
Andry <andrysadonis@gmail.com>
2023-01-23 00:11:09 -0500

    adding new end points
    Adding the endPoint for geolocation reverse and for get the images from the api

--- lib/core/const/end_points.dart
+++ lib/core/const/end_points.dart
@@ -1,5 +1,7 @@
 const geoCodingEndPoint = 'geo/1.0/direct';
 const nextDaysEndPoint = 'data/2.5/forecast';
+const reverseGeoCoding = 'geo/1.0/reverse';
 const currentWeatherEndPoint = 'data/2.5/weather';
 const baseUrl = 'api.openweathermap.org';
 const baseUrlFlags = 'https://countryflagsapi.com/png/';
+const baseUrlImagesOpenWeatherMap = 'https://openweathermap.org/img/wn/';
