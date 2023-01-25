35aaec6a29f2a1900885528a69d19c7a73964225
Andry <andrysadonis@gmail.com>
2023-01-25 13:13:30 -0500

    deleting unnesesary blocs

--- lib/features/current_weather/presentation/bloc/current_weather_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'current_weather_bloc.dart';
-
-abstract class CurrentWeatherEvent {}
-
-class CurrentWeatherStarted extends CurrentWeatherEvent {
-  final double lat, lon;
-  CurrentWeatherStarted(this.lat, this.lon);
-}
